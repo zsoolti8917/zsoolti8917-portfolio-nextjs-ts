7bf29ed3a78cce28fe597359a1701945a40e661f
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-21 19:48:21 +0200

    Changed to nextjs Script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script
+ from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
               <Head>
-              <script defer src="http://zsoltvarjuprojects.com/script.js" data-website-id="6d6ae2f3-05ec-414d-9ab7-9e973974e4d0"></script>
+                
+              <Script defer src="http://zsoltvarjuprojects.com/script.js" data-website-id="6d6ae2f3-05ec-414d-9ab7-9e973974e4d0"></Script>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
