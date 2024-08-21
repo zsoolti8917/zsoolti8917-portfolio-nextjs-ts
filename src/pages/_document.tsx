076a20cf7b523a1c7120ced73bd29095af3ebc49
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-21 19:44:34 +0200

    added umami tracking script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
               <Head>
+              <script defer src="http://zsoltvarjuprojects.com/script.js" data-website-id="6d6ae2f3-05ec-414d-9ab7-9e973974e4d0"></script>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
