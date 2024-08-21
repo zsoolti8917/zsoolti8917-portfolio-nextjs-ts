90d06a7a029c0a24a7a958c1db4d354c60af7d9a
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-21 20:20:24 +0200

    change in umami script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,17 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Script
  from "next/script";
 export default function Document() {
+  console.log("Custom Document is being used");
+
   return (
     <Html lang="en">
               <Head>
                 
-              <Script async src="https://zsoltvarjuprojects.com/script.js" data-website-id="6d6ae2f3-05ec-414d-9ab7-9e973974e4d0"></Script>
+              <Script
+  src="https://zsoltvarjuprojects.com/script.js"
+  data-website-id="6d6ae2f3-05ec-414d-9ab7-9e973974e4d0"
+  strategy="afterInteractive"
+/>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
         </Head>
-      <body id="root" className="bg-zinc-900 text-zinc-50">
+        <body id="root" className="bg-zinc-900 text-zinc-50">
         <Main />
         <NextScript />
       </body>
